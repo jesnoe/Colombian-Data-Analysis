7f0e4115e80db4c93f89ce1be2bbd745d6f740f2
Eunseong Jang <ejang21@umd.edu>
2024-02-29 12:31:24 -0500

    n_obs and waterways

--- Anecdotal network clustering analysis.R
+++ Anecdotal network clustering analysis.R
@@ -7,9 +7,7 @@ library(gridExtra)
 library(lubridate)
 library(colmaps)
 library(sf)
-
-base_to_base  <- read.csv("Colombia Data/Anecdotal base to base with coordinates.csv") %>% as_tibble
-HCl_to_HCl  <- read.csv("Colombia Data/Anecdotal HCl to HCl with coordinates.csv") %>% as_tibble
+library(sp)
 
 armed_groups_combined <- list()
 years <- (2008:2016)[-c(2,8)]
@@ -28,6 +26,7 @@ for (year in years[-1]) {
 
 {
 municipios_capital <- municipios@data %>% mutate(municipio=str_to_upper(municipio, locale="en"))
+municipios_capital$id <- as.numeric(municipios_capital$id)
 municipios_capital$municipio <- stri_trans_general(municipios_capital$municipio, "Latin-ASCII")
 municipios_capital$depto <-  stri_trans_general(municipios_capital$depto, "Latin-ASCII")
 municipios_capital$depto <- gsub(" De ", " de ", municipios_capital$depto)
@@ -63,6 +62,151 @@ price_2016_2021 <- read.csv("Colombia Data/Colombia Price Data 2016-2021 edited.
 # coca_seizures <- left_join(municipios_id, coca_seizures, by="id")
 }
 
+population <- read_xlsx("Colombia Data/Census population by municipios (2018).xlsx") %>% 
+  filter(!is.na(municipio)) %>% 
+  select(-type)
+population <- population %>% 
+  mutate(id=substr(municipio, 1, 5) %>% as.numeric) %>%
+  select(id, population) %>% 
+  right_join(municipios_capital %>% mutate(id=as.numeric(id)), by="id") %>% 
+  select(id, id_depto, municipio, depto, population)
+# write.csv(population, "Colombia Data/Census population by municipios (2018).csv")
+
+price_2016_2021 %>% 
+  group_by(id) %>% 
+  summarise(n_seeds=sum(!is.na(seeds)),
+            n_leaves=sum(!is.na(leaves))) %>% 
+  arrange(desc(n_seeds), desc(n_leaves))
+
+price <- rbind(price_2013_2015, price_2016_2021 %>% select(-seeds, -leaves))
+
+price %>% select(month, year) %>% unique %>% nrow # 86 months (except for 2013 with "Jan to Apr", "May to Aug", "Sep to Dec")
+n_price <- price %>% 
+  group_by(id) %>% 
+  summarise(paste_avg=sum(!is.na(paste_avg)),
+            paste_wholesale=sum(!is.na(paste_wholesale)),
+            paste_retail=sum(!is.na(paste_retail)),
+            base_avg=sum(!is.na(base_avg)),
+            base_wholesale=sum(!is.na(base_wholesale)),
+            base_retail=sum(!is.na(base_retail)),
+            hyd_avg=sum(!is.na(hyd_avg)),
+            hyd_wholesale=sum(!is.na(hyd_wholesale)),
+            hyd_retail=sum(!is.na(hyd_retail)))
+n_price[n_price == 0] <- NA
+n_armed_groups <- n_armed_groups %>% 
+  mutate(n_armed_groups=n_armed_groups %>% select(-id) %>% apply(1, function(x) sum(!is.na(x)))) %>% 
+  select(id, n_armed_groups) %>% 
+  arrange(id)
+
+get_n_obs <- function(dat, data_name) {
+  col_name <- paste0("n_", data_name)
+  dat[[col_name]] <- dat[,-(1:4)] %>% apply(1, function(x) sum(!is.na(x)))
+  return(dat[, c(grep("id", names(dat)), length(names(dat)))] %>% arrange(id))
+}
+
+n_cultivation <- get_n_obs(cultivation, "cultivation")
+n_labs_HCl <- get_n_obs(labs_HCl, "labs_HCl")
+n_labs_PPI <- get_n_obs(labs_PPI, "labs_PPI")
+n_eradication_aerial <- get_n_obs(eradication_aerial, "eradication_aerial")
+n_eradication_manual <- get_n_obs(eradication_manual, "eradication_manual")
+n_coca_seizures <- get_n_obs(coca_seizures, "coca_seizures")
+n_base_seizures <- get_n_obs(base_seizures, "base_seizures")
+n_HCl_seizures <- get_n_obs(HCl_seizures, "HCl_seizures")
+
+n_obs_list_municipios <- municipios_capital %>% mutate(id=as.numeric(id)) %>% arrange(id) %>% 
+  left_join(n_cultivation, by="id") %>% 
+  left_join(n_labs_HCl, by="id") %>% 
+  left_join(n_labs_PPI, by="id") %>% 
+  left_join(n_eradication_aerial, by="id") %>% 
+  left_join(n_eradication_manual, by="id") %>% 
+  left_join(n_coca_seizures, by="id") %>%
+  left_join(n_base_seizures, by="id") %>% 
+  left_join(n_HCl_seizures, by="id") %>% 
+  left_join(n_armed_groups, by="id") %>% 
+  left_join(n_price, by="id") %>% 
+  mutate(non_missing_cols=n_obs_list_municipios %>% apply(1, function(x) sum(!is.na(x[5:22])))) %>% 
+  mutate(non_missing_prices=n_obs_list_municipios %>% apply(1, function(x) sum(!is.na(x[14:22]), na.rm=T))) %>% 
+  as_tibble
+# write.csv(n_obs_list_municipios, "Colombia Data/num of observations (municipios).csv", row.names=F)
+
+read.csv("Colombia Data/N of observations (municipios).csv") %>% as_tibble
+
+n_obs_list_depto <- n_obs_list_municipios %>%
+  group_by(id_depto) %>% 
+  summarise(depto=depto[1],
+            n_cultivation=max(n_cultivation, na.rm=T),
+            n_labs_HCl=max(n_labs_HCl, na.rm=T),
+            n_labs_PPI=max(n_labs_PPI, na.rm=T),
+            n_eradication_aerial=max(n_eradication_aerial, na.rm=T),
+            n_eradication_manual=max(n_eradication_manual, na.rm=T),
+            n_coca_seizures=max(n_coca_seizures, na.rm=T),
+            n_base_seizures=max(n_base_seizures, na.rm=T),
+            n_HCl_seizures=max(n_HCl_seizures, na.rm=T),
+            n_armed_groups=max(n_armed_groups, na.rm=T),
+            paste_avg=max(paste_avg, na.rm=T),
+            paste_wholesale=max(paste_wholesale, na.rm=T),
+            paste_retail=max(paste_retail, na.rm=T),
+            base_avg=max(base_avg, na.rm=T),
+            base_wholesale=max(base_wholesale, na.rm=T),
+            base_retail=max(base_retail, na.rm=T),
+            hyd_avg=max(hyd_avg, na.rm=T),
+            hyd_wholesale=max(hyd_wholesale, na.rm=T),
+            hyd_retail=max(hyd_retail, na.rm=T),
+            non_missing_cols=max(non_missing_cols, na.rm=T),
+            non_missing_prices=max(non_missing_prices, na.rm=T)
+  )
+
+# write.csv(n_obs_list_depto, "Colombia Data/n of observations (department).csv", row.names=F)
+
+# convert anecdotal data cities into municipios
+base_to_base  <- read.csv("Colombia Data/Anecdotal base to base with id.csv") %>% as_tibble
+HCl_to_HCl  <- read.csv("Colombia Data/Anecdotal HCl to HCl with coordinates.csv") %>% as_tibble
+anecdotal_annual  <- read.csv("Colombia Data/Anecdotal annual.csv") %>% as_tibble
+anecdotal_annual %>% select(SOURCE.MUNICIPIO.CITY, SOURCE.DEPARTAMENTO) %>% unique
+anecdotal_annual %>% select(DESTINATION..CITY., DESTINATION.DEPARTAMENTO) %>% unique
+
+map <- municipios
+map_df <- suppressMessages(fortify(map)) %>% 
+  mutate(id=as.numeric(id)) %>% 
+  filter(!(id %in% c(88001, 88564))) # excludes islands in Caribbean
+map_df <- left_join(map_df, municipios_capital %>% select(id, municipio, depto) %>% unique, by="id")
+
+for (i in 1:nrow(HCl_to_HCl)) {
+  source_long_i <- HCl_to_HCl$source_long[i]
+  source_lat_i <- HCl_to_HCl$source_lat[i]
+  destination_long_i <- HCl_to_HCl$destine_long[i]
+  destination_lat_i <- HCl_to_HCl$destine_lat[i]
+  # mun_index <- which(abs(source_long_i - map_df$long) < 0.005)
+  # candidate_mun <- map_df[mun_index,]
+  # candidate_mun_minmax <- candidate_mun %>%
+  #   group_by(id) %>%
+  #   summarise(min_lat = min(lat), max_lat=max(lat), municipio=municipio[1], depto=depto[1])
+  search_index <- c(0,0)
+  for (id_i in (unique(municipios_capital$id) %>% sort)) {
+    municipio_id <- map_df %>% filter(id == id_i)
+    
+    if (point.in.polygon(source_long_i, source_lat_i, municipio_id$long, municipio_id$lat)) {
+      source_id <- id_i
+      search_index[1] <- 1
+    }
+    if (point.in.polygon(destination_long_i, destination_lat_i, municipio_id$long, municipio_id$lat)) {
+      destination_id <- id_i
+      search_index[2] <- 1
+    }
+    
+    if (sum(search_index) == 2) break
+  }
+  # candidate_index <- which(source_lat_i >= candidate_mun_minmax$min_lat & source_lat_i <= candidate_mun_minmax$max_lat)
+  # matched_municipio <- candidate_mun_minmax[candidate_index[which.max(candidate_mun_minmax$min_lat[candidate_index])],]
+  HCl_to_HCl$source_id[i] <- source_id
+  HCl_to_HCl$source_municipio[i] <- municipios_capital %>% filter(id == source_id) %>% pull(municipio)
+  HCl_to_HCl$destination_id[i] <- destination_id
+  HCl_to_HCl$destination_municipio[i] <- municipios_capital %>% filter(id == destination_id) %>% pull(municipio)
+}
+
+HCl_to_HCl[which(is.na(HCl_to_HCl$source_id)),] %>% as.data.frame
+
+# map checking
 base_to_base_source_id <- base_to_base %>% filter(!is.na(source_id)) %>% pull(source_id) %>% unique
 base_to_base_destination_id <- base_to_base %>% filter(!is.na(destination_id)) %>% pull(destination_id) %>% unique
 
---
+++ Waterways.R
@@ -0,0 +1,122 @@
+waterways <- st_read("Colombia Data/gis_osm_waterways_free_1.shp")
+waterways$fclass %>% unique
+waterways$geometry[1]
+
+st_layers("Colombia Data/gis_osm_waterways_free_1.shp")
+
+map <- departamentos
+map_df <- suppressMessages(fortify(map)) %>% 
+  mutate(id_depto=as.numeric(id)) %>% 
+  filter(id_depto != 88) # excludes islands in Caribbean
+
+empty_map <- ggplot(map_df, aes(x=long, y=lat)) + 
+  geom_polygon(aes(group=group),
+               color = "black",
+               fill = "white",
+               linewidth = 0.3) + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  coord_quickmap() +
+  labs(fill="", x="", y="", title="") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank(),
+        legend.position="none"
+  )
+
+table(waterways$fclass)
+
+waterway_map <- waterways %>% ggplot() + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  geom_sf(color="blue", linewidth = 0.2) +
+  geom_polygon(data=map_df,
+               aes(x=long,
+                   y=lat,
+                   group=group),
+               color = "black",
+               fill = NA,
+               linewidth = 0.1) + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  labs(fill="", x="", y="", title="All Waterways") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank(),
+        legend.position="none"
+  )
+ggsave("Colombia Data/Figs/waterways.png", waterway_map, scale=1)
+
+
+river_map <- rivers %>% ggplot() + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  geom_sf(color="blue", linewidth = 0.2) +
+  geom_polygon(data=map_df,
+               aes(x=long,
+                   y=lat,
+                   group=group),
+               color = "black",
+               fill = NA,
+               linewidth = 0.1) + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  labs(fill="", x="", y="", title="All Rivers") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank(),
+        legend.position="none"
+  )
+ggsave("Colombia Data/Figs/rivers.png", river_map, scale=1)
+
+broad_river_map <- rivers %>%
+  filter(width > 0) %>% 
+  ggplot() + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  geom_sf(color="blue", linewidth = 0.2) +
+  geom_polygon(data=map_df,
+               aes(x=long,
+                   y=lat,
+                   group=group),
+               color = "black",
+               fill = NA,
+               linewidth = 0.1,
+               alpha=1) + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  labs(fill="", x="", y="", title="Rivers (Width > 0)") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank(),
+        legend.position="none"
+  )
+ggsave("Colombia Data/Figs/broad rivers.png", broad_river_map, scale=1)
+
+canals <- waterways %>% filter(fclass == "canal")
+canal_map <- canals %>% ggplot() + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  geom_sf(color="blue", linewidth = 0.2) +
+  geom_polygon(data=map_df,
+               aes(x=long,
+                   y=lat,
+                   group=group),
+               color = "black",
+               fill = NA,
+               linewidth = 0.1) + 
+  expand_limits(x = map_df$long, y = map_df$lat) + 
+  labs(fill="", x="", y="", title="Canals") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank(),
+        legend.position="none"
+  )
+ggsave("Colombia Data/Figs/canals.png", canal_map, scale=1)
