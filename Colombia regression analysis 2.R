3b09a31cd5fc8764ae6aef6c4d2012c80255d34b
Eunseong Jang <ejang21@umd.edu>
2024-04-18 13:10:00 -0400

    separability check

--- Colombia regression analysis 2.R
+++ Colombia regression analysis 2.R
@@ -425,6 +425,11 @@ labs_PPI_2steps_year <- labs_PPI_reg_data %>%
          erad_manual=ifelse(is.na(erad_manual), 0, erad_manual),
          coca_seizures=ifelse(is.na(coca_seizures), 0, coca_seizures),
          base_seizures=ifelse(is.na(base_seizures), 0, base_seizures)) %>% 
+  left_join(labs_HCl_reg_data %>%
+              filter(year == ex_year) %>%
+              mutate(n_HCl_labs=n_labs) %>%
+              select(id, n_HCl_labs), by="id") %>% 
+  mutate(n_HCl_labs=ifelse(is.na(n_HCl_labs), 0, n_HCl_labs)) %>% 
   left_join(armed_groups[[paste0("y", ex_year)]] %>% select(-(municipio:n_armed_groups)), by="id")
 labs_PPI_2steps_year[is.na(labs_PPI_2steps_year)] <- 0
 
@@ -433,6 +438,16 @@ sum(!(anecdotal_year %>% filter(PROCESS == "BASE") %>% pull(source_id) %>% uniqu
 
 {
 data_year <- labs_PPI_2steps_year %>% left_join(population %>% select(id, population), by="id")
+  
+names(data_year) <- gsub(" ", "_", names(data_year)) %>% stri_trans_general("Latin-ASCII") 
+names(data_year)[15:37] <- c("Los_Rastrojos", "Clan_del_Golfo", "Las_Aguilas_Negras", "Los_Paisas",
+                             "Ejercito_Revolucionario_Popular_Antisubversivo_de_Colombia", "Los_Pachenga_O_AUTODEFENSAS_CONQUISTADORAS_DE_LA_SIERRA_NEVADA",
+                             "Los_Caparros_FRENTE_VIRGILIO_PERALTA_ARENAS", "Los_Pachelly", "Los_Puntilleros", "La_Constru", "Los_Contadores",
+                             "La_Oficina_de_Envigado_DEL_VALLE_DE_ABURRA_U_OVA", "Los_Pelusos", "Libertadores_del_Nordeste_NUEVO_RENACER_O_GUEROS", "La_Cordillera",
+                             "La_Empresa", "La_Local", "Los_Caquetenos", "Los_Costenos", "Nuevo_Orden", "HEROES_DEL_CENTRAL_BOLIVAR_BAJO_CAUCA",
+                             "Grupos_no_identificados", "Autodefensas_Unidas_de_Colombia")
+
+data_year$base_source_all <- ifelse(data_year$id %in% (base_to_base %>% pull(source_id) %>% unique), 1, 0) %>% as.factor
 data_year$base_source <- ifelse(data_year$id %in% (anecdotal_year %>% filter(PROCESS == "BASE") %>% pull(source_id)), 1, 0) %>% as.factor
 data_year$base_destination <- ifelse(data_year$id %in% (anecdotal_year %>% filter(PROCESS == "BASE") %>% pull(destination_id)), 1, 0) %>% as.factor
 data_year$HCl_source <- ifelse(data_year$id %in% (anecdotal_year %>% filter(PROCESS == "COCAINE") %>% pull(source_id)), 1, 0) %>% as.factor
@@ -504,37 +519,98 @@ data_year_general_destination3 <- data_year[c(data_year_general_destination1_ind
 
 ## regression with data_year (population and rivers)
   # base source/destination
-base_source_glm <- glm(base_source~., family="binomial",  data=data_year %>% select(n_labs:population, base_source))
+base_source_glm <- glm(base_source~., family="binomial",
+                       data=data_year %>%
+                         # mutate(n_labs=ifelse(n_labs > 0, 1, 0)) %>% 
+                         select(n_labs:population, base_source))
 summary(base_source_glm)
 
-base_source_year <- data_year %>%
-  select(n_labs:population, base_source) %>% 
-  mutate(posterior_glm=base_source_glm$fitted.values)
-base_source_year %>% filter(posterior_glm < .5) %>% select(base_source, posterior_glm) %>% pull(base_source) %>% table
-base_source_year %>% filter(posterior_glm >= .5) %>% select(base_source, posterior_glm) %>% pull(base_source) %>% table
-base_source_year %>% filter(posterior_glm < .5 & base_source == 1) %>% select(base_source, posterior_glm) %>% arrange(posterior_glm)
+base_source_glm2 <- glm(base_source~., family="binomial",
+                        data=data_year %>%
+                          mutate(coca_group=(Clan_del_Golfo + Los_Pelusos)) %>%
+                          select(n_labs:base_seizures, -erad_aerial, coca_group, population, base_source))
+summary(base_source_glm2)
 
-names(data_year) <- gsub(" ", "_", names(data_year)) %>% stri_trans_general("Latin-ASCII") 
-names(data_year)[14:36] <- c("Los_Rastrojos", "Clan_del_Golfo", "Las_Aguilas_Negras", "Los_Paisas",
-                             "Ejercito_Revolucionario_Popular_Antisubversivo_de_Colombia", "Los_Pachenga_O_AUTODEFENSAS_CONQUISTADORAS_DE_LA_SIERRA_NEVADA",
-                             "Los_Caparros_FRENTE_VIRGILIO_PERALTA_ARENAS", "Los_Pachelly", "Los_Puntilleros", "La_Constru", "Los_Contadores",
-                             "La_Oficina_de_Envigado_DEL_VALLE_DE_ABURRA_U_OVA", "Los_Pelusos", "Libertadores_del_Nordeste_NUEVO_RENACER_O_GUEROS", "La_Cordillera",
-                             "La_Empresa", "La_Local", "Los_Caquetenos", "Los_Costenos", "Nuevo_Orden", "HEROES_DEL_CENTRAL_BOLIVAR_BAJO_CAUCA",
-                             "Grupos_no_identificados", "Autodefensas_Unidas_de_Colombia")
-base_source_rf <- randomForest(base_source~., data=data_year %>% select(n_labs:population, base_source), type="classification", ntree=50, mtry=20)
+base_source_glm_reduced <- glm(base_source~., family="binomial",
+                        data=data_year %>%
+                          mutate(coca_group=(Los_Rastrojos + Clan_del_Golfo + Los_Pelusos)) %>%
+                          select(n_labs:coca_seizures, -base_price_distance, -erad_aerial, -erad_manual, coca_group, population, base_source))
+summary(base_source_glm_reduced)
+
+base_source_year <- data_year %>%
+  select(id, n_labs:population, base_source) %>% 
+  mutate(coca_group=(Los_Rastrojos + Clan_del_Golfo + Los_Pelusos)) %>%
+  mutate(posterior_glm=base_source_glm$fitted.values) %>% 
+  mutate(posterior_glm2=base_source_glm2$fitted.values) %>% 
+  mutate(posterior_glm_r=base_source_glm_reduced$fitted.values)
+
+threshold <- .1
+base_source_year %>% filter(posterior_glm < threshold) %>% select(base_source, posterior_glm) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_glm >= threshold) %>% select(base_source, posterior_glm) %>% pull(base_source) %>% table
+
+base_source_year %>% filter(posterior_glm2 < threshold) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_glm2 >= threshold) %>% pull(base_source) %>% table # lower true 1
+
+base_source_year %>% filter(posterior_glm_r < threshold) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_glm_r >= threshold) %>% pull(base_source) %>% table # lower true 1
+
+base_source_year %>% 
+  filter(posterior_glm < threshold & base_source == 1) %>%
+  select(id, n_labs:n_HCl_labs, -erad_aerial, coca_group, population, posterior_glm2)
+
+base_source_year %>% 
+  filter(posterior_glm2 < threshold & base_source == 1) %>%
+  select(id, n_labs:n_HCl_labs, -erad_aerial, coca_group, population, posterior_glm2)
+
+base_source_year %>% 
+  filter(posterior_glm > threshold) %>%
+  select(id, n_labs:n_HCl_labs, -erad_aerial, coca_group, population, posterior_glm)
+
+base_source_year %>% 
+  filter(posterior_glm2 > threshold) %>%
+  select(id, n_labs:n_HCl_labs, -erad_aerial, coca_group, population, posterior_glm2)
+
+base_source_year %>% filter(posterior_glm < threshold & base_source == 1) %>% select(base_source, posterior_glm) %>% arrange(posterior_glm)
+
+set.seed(291038)
+base_source_rf <- randomForest(base_source~.,
+                               data=data_year %>%
+                                 select(n_labs:population, base_source),
+                               type="classification", ntree=50, mtry=20)
+
+base_source_rf2 <- randomForest(base_source~.,
+                                data=data_year %>%
+                                  mutate(coca_group=(Los_Rastrojos + Clan_del_Golfo + Los_Pelusos)) %>%
+                                  select(n_labs:base_seizures, coca_group, population, base_source),
+                                type="classification", ntree=50, mtry=10)
 base_source_year$posterior_rf <- base_source_rf$votes[,2]
-base_source_year %>% filter(posterior_rf < .5) %>% select(base_source, posterior_rf) %>% pull(base_source) %>% table
-base_source_year %>% filter(posterior_rf >= .5) %>% select(base_source, posterior_rf) %>% pull(base_source) %>% table
-base_source_year %>% filter(posterior_rf < .5 & base_source == 1) %>% select(base_source, posterior_rf) %>% arrange(posterior_rf)
+base_source_year$posterior_rf2 <- base_source_rf2$votes[,2]
+
+threshold <- .1
+base_source_year %>% filter(posterior_rf < threshold) %>% select(base_source, posterior_rf) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_rf >= threshold) %>% select(base_source, posterior_rf) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_rf < threshold & base_source == 1) %>% select(base_source, posterior_rf) %>% arrange(posterior_rf)
+
+base_source_year %>% filter(posterior_rf2 < threshold) %>% select(base_source, posterior_rf2) %>% pull(base_source) %>% table # worse than individual groups
+base_source_year %>% filter(posterior_rf2 >= threshold) %>% select(base_source, posterior_rf2) %>% pull(base_source) %>% table
+base_source_year %>% filter(posterior_rf2 < threshold & base_source == 1) %>% select(base_source, posterior_rf2) %>% arrange(posterior_rf2)
 
 base_source_year <- base_source_year %>% 
-  mutate(y_glm=ifelse(posterior_glm < .5, 0, 1),
-         y_rf=ifelse(posterior_rf < .5, 0, 1))
+  mutate(y_glm=ifelse(posterior_glm < threshold, 0, 1),
+         y_glm2=ifelse(posterior_glm2 < threshold, 0, 1),
+         y_rf=ifelse(posterior_rf < threshold, 0, 1),
+         y_rf2=ifelse(posterior_rf2 < threshold, 0, 1))
 
 base_source_year %>%
-  filter(y_glm != y_rf) %>% 
-  select(base_source, posterior_glm, posterior_rf)
-sum(base_source_year$base_source %>% as.numeric - 1) # 50
+  filter(y_glm != y_rf2) %>% 
+  select(base_source, y_glm, y_rf2, posterior_glm, posterior_rf2) %>% 
+  print(n=20)
+
+base_source_year %>%
+  filter(y_glm2 != y_rf2) %>% 
+  select(base_source, posterior_glm2, posterior_rf2)
+
+sum(base_source_year$base_source == 1) # 50
 
 data_year_base_source1_glm <- glm(base_source~., family="binomial",  data=data_year_base_source1 %>% select(n_labs:population, base_source))
 summary(data_year_base_source1_glm)
@@ -553,12 +629,32 @@ data_year_base_destination3_glm <- glm(base_destination~., family="binomial",  d
 summary(data_year_base_destination3_glm)
 
 
+## kmeans clustering
+set.seed(50)
+base_source_kmeans <- kmeans(data_year %>%
+                               mutate(coca_group=(Los_Rastrojos + Clan_del_Golfo + Los_Pelusos)) %>%
+                               select(n_labs:base_seizures, -erad_aerial, coca_group, population) %>% 
+                               scale,
+                             4)
+
+for (i in 1:length(base_source_kmeans$size)) {
+  print(paste("cluster", i, ":", sum(base_source_year$base_source == 1 & as.numeric(base_source_kmeans$cluster == i) == 1)))
+}
+base_source_kmeans$size
+
+base_source_year$cluster <- as.numeric(base_source_kmeans$cluster == 3)
+table(base_source_year$base_source)
+table(base_source_year$cluster)
+
+base_source_year %>% 
+  filter(base_source == 1 & cluster == 0) %>% 
+  select(id, n_labs:n_HCl_labs, -erad_aerial, coca_group, population, posterior_glm)
 
 F1_PU <- function(glm_model, test_data, positive) {
   n_data <- nrow(test_data)
   positive <- as.numeric(positive)
   n_positive <- sum(positive, na.rm=T)
-  y_hat <- ifelse(predict(glm_model, test_data) > .5, 1, 0)
+  y_hat <- ifelse(predict(glm_model, test_data) > threshold, 1, 0)
   recall <- sum(positive*y_hat, na.rm=T)/n_positive
   result <- recall^2/(sum(y_hat/n_data, na.rm=T))
   return(result)
