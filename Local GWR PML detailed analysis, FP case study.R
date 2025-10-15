5eccfbed3914933f6d081c8b0fcac6d845c35dbc
unknown <esjang1379@gmail.com>
2025-10-15 16:38:49 -0400

    local GWR with data neighbors

--- Local GWR PML detailed analysis, FP case study.R
+++ Local GWR PML detailed analysis, FP case study.R
@@ -15,6 +15,8 @@ library(pROC)
 library(glmnet)
 library(reshape2)
 library(regclass)
+library(logistf)
+library(distances)
 {
 municipios_capital <- municipios@data %>% mutate(municipio=str_to_upper(municipio, locale="en"))
 municipios_capital$id <- as.numeric(municipios_capital$id)
@@ -59,6 +61,7 @@ empty_map <- ggplot(map_df, aes(x=long, y=lat)) +
 base_gwr_data <- read.csv("Colombia Data/base gwr data.csv") %>% as_tibble
 hyd_gwr_data <- read.csv("Colombia Data/hyd gwr data.csv") %>% as_tibble
 gwr_data_dist <- read.csv("Colombia Data/gwr data dist.csv") %>% as_tibble
+influence_tbl <- read.csv("Colombia Data/influence_tbl.csv") %>% as_tibble
 
 PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest <- read.csv("Colombia Data/local GWR PML result predicted prices/local GWR PML coefs hyd_destination leave-one-out PML_log_seizure_coca_bw_F1 all var drop 10 (08-20-2025).csv") %>% as_tibble
 PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_source <- read.csv("Colombia Data/local GWR PML result predicted prices/local GWR PML coefs hyd_source leave-one-out PML_log_seizure_coca_bw_F1 all var drop 10 weight 7-3 (09-08-2025).csv") %>% as_tibble
@@ -126,19 +129,21 @@ check_most_influential_var(23419, PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_
 PML_GWR_pred_10_loo_hyd_dest <- PML_GWR_pred_10_loo_hyd_dest %>% mutate(prediction = ifelse(y == 0 & y_PML_var_drop_loo == 1, "FP",
                                                                                             ifelse(y == 1 & y_PML_var_drop_loo == 1, "TP",
                                                                                                    ifelse(y == 1 & y_PML_var_drop_loo == 0, "FN", "TN"))) %>% as.factor)
-influence_tbl <- tibble(var_name = names(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest)[-(1:3)])
-location_tbl <- tibble()
-for (i in 1:nrow(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest)) {
-  id_i <- PML_GWR_pred_10_loo_hyd_dest$id[i]
-  list_i <- check_most_influential_var(id_i, PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest, hyd_gwr_data)
-  influence_tbl <- bind_cols(influence_tbl, list_i$influence)
-  location_tbl <- bind_rows(location_tbl, list_i$location)
-}
-var_names <- influence_tbl$var_name
-influence_tbl <- as_tibble(t(influence_tbl[,-1]))
-names(influence_tbl) <- var_names
-influence_tbl <- bind_cols(location_tbl[,-(4:5)], influence_tbl) %>% mutate(pi_hat = PML_GWR_pred_10_loo_hyd_dest$PML_gwr_pi_hat_var_drop_loo) %>% relocate(id:depto, pi_hat)
-influence_tbl <- influence_tbl %>% left_join(PML_GWR_pred_10_loo_hyd_dest %>% select(id, prediction), by="id") %>% relocate(id:depto, prediction)
+# influence_tbl <- tibble(var_name = names(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest)[-(1:3)])
+# location_tbl <- tibble()
+# for (i in 1:nrow(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest)) {
+#   id_i <- PML_GWR_pred_10_loo_hyd_dest$id[i]
+#   list_i <- check_most_influential_var(id_i, PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest, hyd_gwr_data)
+#   influence_tbl <- bind_cols(influence_tbl, list_i$influence)
+#   location_tbl <- bind_rows(location_tbl, list_i$location)
+# }
+# var_names <- influence_tbl$var_name
+# influence_tbl <- as_tibble(t(influence_tbl[,-1]))
+# names(influence_tbl) <- var_names
+# influence_tbl <- bind_cols(location_tbl[,-(4:5)], influence_tbl) %>% mutate(pi_hat = PML_GWR_pred_10_loo_hyd_dest$PML_gwr_pi_hat_var_drop_loo) %>% relocate(id:depto, pi_hat)
+# influence_tbl <- influence_tbl %>% left_join(PML_GWR_pred_10_loo_hyd_dest %>% select(id, prediction), by="id") %>% relocate(id:depto, prediction)
+# write.csv(influence_tbl, "Colombia Data/influence_tbl.csv", row.names = F)
+
 influence_tbl
 influence_tbl_FP <- influence_tbl %>% filter(prediction == "FP") %>% arrange(desc(pi_hat))
 influence_tbl_TP <- influence_tbl %>% filter(prediction == "TP") %>% arrange(desc(pi_hat))
@@ -200,26 +205,131 @@ for (i in 1:5) {
   id_i <- influence_tbl_FP$id[i]
   j <- which(hyd_gwr_data$id == id_i)
   neighbors_id <- hyd_gwr_data$id[order(gwr_data_dist[j,] %>% t)[2:6]]
-  location_map <- ggplot(map_df %>% mutate(neighbors = ifelse(id == id_i, "major city",
-                                                              ifelse(id %in% neighbors_id, "neighbors", "else"))),
-                         aes(x=long, y=lat)) + 
-    geom_polygon(aes(group=group, fill=neighbors),
-                 color = "black",
-                 linewidth = 0.1) + 
-    expand_limits(x = map_df$long, y = map_df$lat) + 
-    coord_quickmap() +
-    scale_fill_manual(values = c("else" = "white", "neighbors" = "deepskyblue3", "major city" = "red")) +
-    labs(fill=major_cities[i], x="", y="", title="") +
-    theme_bw() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.border = element_blank(),
-          axis.text = element_blank(),
-          line = element_blank()
-    )
-  ggsave(sprintf("Colombia Data/local GWR PML result predicted prices/FP location maps/location %s, %s.png", influence_tbl_FP$municipio[i], influence_tbl_FP$depto[i]), location_map, scale=1)
+  # location_map <- ggplot(map_df %>% mutate(neighbors = ifelse(id == id_i, "major city",
+  #                                                             ifelse(id %in% neighbors_id, "neighbors", "else"))),
+  #                        aes(x=long, y=lat)) + 
+  #   geom_polygon(aes(group=group, fill=neighbors),
+  #                color = "black",
+  #                linewidth = 0.1) + 
+  #   expand_limits(x = map_df$long, y = map_df$lat) + 
+  #   coord_quickmap() +
+  #   scale_fill_manual(values = c("else" = "white", "neighbors" = "deepskyblue3", "major city" = "red")) +
+  #   labs(fill=major_cities[i], x="", y="", title="") +
+  #   theme_bw() +
+  #   theme(panel.grid.major = element_blank(),
+  #         panel.grid.minor = element_blank(),
+  #         panel.border = element_blank(),
+  #         axis.text = element_blank(),
+  #         line = element_blank()
+  #   )
+  # ggsave(sprintf("Colombia Data/local GWR PML result predicted prices/FP location maps/location %s, %s.png", influence_tbl_FP$municipio[i], influence_tbl_FP$depto[i]), location_map, scale=1)
   print(bind_rows(influence_tbl_FP[i,], influence_tbl %>% filter(id %in% neighbors_id) %>% arrange(id)) %>% mutate(pi_hat = round(pi_hat, 3)))
   print(bind_rows(hyd_gwr_data[j,], hyd_gwr_data %>% filter(id %in% neighbors_id) %>% arrange(id)))
   print(bind_rows(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest %>% filter(id == id_i),
                   PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest %>% filter(id %in% neighbors_id) %>% arrange(id)))
 }
+
+
+
+# adding data-distance neighbors for overffiting cases
+  # local_GWR_coefs_PML_hyd_dest_var_drop_log_seizure_scaled
+load("Colombia Data/local GWR PML result predicted prices/local GWR PML hyd_dest leave-one-out all var drop log seizure coca scaled n_drop=10 (08-20-2025).RData")
+hyd_gwr_data_dist <- dist(hyd_gwr_data %>% select(-(id:y))) %>% as.matrix
+hyd_gwr_data %>% filter(id == id_i)
+
+id_i <- 44090 # opposite influence of seizures compared to Santa Marta, Magdalena (a major city)
+id_i <- 27001
+id_i <- 50287
+id_i <- 25851
+
+local_GWR_data_neighbors <- function(id_i, n_neighbors) {
+  j <- which(hyd_gwr_data$id == id_i)
+  k <- which(PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest$id == id_i)
+  bw_i <- PML_gwr_coefs_F1_var_drop_log_seizure_coca_10_loo_hyd_dest$bw[k]
+  
+  neighbors <- hyd_gwr_data[which(gwr_data_dist[j,] <= bw_i),] %>% filter(id != id_i)
+  hyd_gwr_data_i <- hyd_gwr_data %>% filter(id == id_i)
+  
+  local_GWR_i <- local_GWR_coefs_PML_hyd_dest_var_drop_log_seizure_scaled[[paste0("id_", id_i)]][[paste0("bw_", bw_i)]]
+  model_vars_i <- (local_GWR_i$coefficients %>% names)[-1]
+  hyd_gwr_data_dist <- dist(hyd_gwr_data %>% select(all_of(model_vars_i))) %>% as.matrix
+  ### variables have different scale (normalized, logged, and binary): how can I measure the deviation fairly?
+  abs(neighbors %>% select(all_of(model_vars_i)) %>% apply(2, function(x) min(x)) - (hyd_gwr_data_i %>% select(all_of(model_vars_i)) %>% t %>% as.vector))
+  abs(neighbors %>% select(all_of(model_vars_i)) %>% apply(2, function(x) max(x)) - (hyd_gwr_data_i %>% select(all_of(model_vars_i)) %>% t %>% as.vector))
+  hyd_gwr_data_dist <- distances(hyd_gwr_data %>% select(-(id:y))) %>% as.matrix
+  
+  data_neighbors_id <- hyd_gwr_data$id[order(hyd_gwr_data_dist[j,] %>% t)[2:(n_neighbors+1)]]
+  data_neighbors <- hyd_gwr_data %>% filter(id %in% data_neighbors_id)
+  neighbors_i <- bind_rows(neighbors, data_neighbors)
+  var_names_i <- names(neighbors_i)[names(neighbors_i) %in% model_vars_i]
+  
+  model_i <- logistf(y~., neighbors_i %>% select(y, all_of(var_names_i)), alpha = 0.1)
+  model_include_i <- logistf(y~., bind_rows(neighbors_i, hyd_gwr_data_i) %>% select(y, all_of(var_names_i)), alpha = 0.1)
+  
+  data_pred_i <- hyd_gwr_data %>% filter(id == id_i) %>% select(y, all_of(var_names_i)) %>% relocate(y, model_vars_i)
+  pi_hat_i <- predict(model_i, data_pred_i, type="response")
+  
+  data_neighbors_index <- which(hyd_gwr_data$id %in% data_neighbors_id)
+  data_neighbors_dist <- tibble(id = hyd_gwr_data$id[data_neighbors_index], dist = hyd_gwr_data_dist[j,data_neighbors_index])
+  
+  print(hyd_gwr_data %>% filter(id %in% c(id_i, data_neighbors_id)) %>% left_join(data_neighbors_dist, by="id"))
+  print(local_GWR_i)
+  print(model_i)
+  print(model_include_i)
+  print(pi_hat_i)
+}
+hyd_gwr_data %>% filter(id %in% c(id_i, data_neighbors_id)) %>% left_join(data_neighbors_dist, by="id")
+local_GWR_i
+model_i
+pi_hat_i
+
+local_GWR_data_neighbors(27001, 5)
+local_GWR_data_neighbors(27001, 1)
+local_GWR_data_neighbors(27001, 2)
+local_GWR_data_neighbors(27001, 3)
+local_GWR_data_neighbors(27001, 4)
+local_GWR_data_neighbors(27001, 6)
+local_GWR_data_neighbors(27001, 7)
+local_GWR_data_neighbors(27001, 8)
+local_GWR_data_neighbors(27001, 9)
+local_GWR_data_neighbors(27001, 10)
+
+local_GWR_data_neighbors(50287, 5)
+local_GWR_data_neighbors(50287, 1)
+local_GWR_data_neighbors(50287, 2)
+local_GWR_data_neighbors(50287, 3)
+local_GWR_data_neighbors(50287, 4)
+local_GWR_data_neighbors(50287, 6)
+local_GWR_data_neighbors(50287, 7)
+local_GWR_data_neighbors(50287, 8)
+local_GWR_data_neighbors(50287, 9)
+local_GWR_data_neighbors(50287, 10)
+
+neighbors_id <- bind_rows(hyd_gwr_data %>% filter(id == id_i), data_neighbors) 
+ggplot(map_df %>% mutate(neighbors = ifelse(id == id_i, "Quibdo",
+                                                               ifelse(id %in% neighbors_id$id, "data neighbors", "else"))),
+                       aes(x=long, y=lat)) +
+  geom_polygon(aes(group=group, fill=neighbors),
+               color = "black",
+               linewidth = 0.1) +
+  expand_limits(x = map_df$long, y = map_df$lat) +
+  coord_quickmap() +
+  scale_fill_manual(values = c("else" = "white", "data neighbors" = "deepskyblue3", "Quibdo" = "red")) +
+  labs(fill="", x="", y="", title="") +
+  theme_bw() +
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_blank(),
+        axis.text = element_blank(),
+        line = element_blank()
+  )
+
+
+check_id <- id_i
+coef_id <- model_i$coefficients[-1]
+# location <- municipio_centroid %>% filter(id==check_id)
+influence <- coef_id * data_pred_i[-1]
+influence
+
+id_i <- 27001 # population coef decreased, but still influential
+id_i <- 44090 # population is no longer dominantly influential
